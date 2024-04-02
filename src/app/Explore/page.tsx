fc09812288bafdc2b62d375f858190667cd159e3
viper-271 <adityap2169@gmail.com>
2024-04-02 12:29:52 +0530

    Your commit message

--- src/app/Explore/page.tsx
+++ src/app/Explore/page.tsx
@@ -1,10 +1,13 @@
-
+import  Projectpage  from "@/components/Projectpage";
 
 function Explore() {
     return (
       <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.2]">
         
-      </main>
+       <Projectpage/>
+        
+
+    </main>
     );
   }
 
